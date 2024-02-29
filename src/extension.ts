487ec0799601f6f2743cb6b209ad7c3fdfde0944
William Dumont <william.dumont@grafana.com>
2024-02-29 11:35:48 +0100

    lsp support basic autcompletion example

---
+++ src/extension.ts
@@ -0,0 +1,54 @@
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+    // The server is implemented in node
+    let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+    // The debug options for the server
+    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise, the run options are used
+    let serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions,
+        },
+    };
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'river' }],
+        synchronize: {
+            // Synchronize the setting section 'riverLanguageServer' to the server
+            configurationSection: 'riverLanguageServer',
+            // Notify the server about file changes to '.clientrc' files contained in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+        },
+    };
+
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'riverLanguageServer',
+        'River Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    // Start the client and add it to the subscriptions for proper disposal
+    client.start();
+    context.subscriptions.push(client);
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
